e973f7ec27a3d8a6603b7b51e0e3ce2a4281e5cc
forfd8960 <forfd8960@gmail.com>
2023-07-15 22:19:22 +0800

    Implement grpc server PubMessage and SubQueue

--- queue_server.go
+++ queue_server.go
@@ -1,8 +1,24 @@
 package simplequeue
 
 import (
+	"context"
+	"log"
 	"net"
 	"sync"
+	"time"
+
+	"github.com/forfd8960/simplequeue/pb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	errInvalidPubRequest = func(pubReq *pb.PubMessageRequest) error {
+		return status.Errorf(codes.InvalidArgument, "Invalid Pub Message Request: %v", pubReq)
+	}
+	errInvalidArgument = func(msg string, args ...any) error {
+		return status.Errorf(codes.InvalidArgument, msg, args)
+	}
 )
 
 type QueueServer struct {
@@ -49,4 +65,43 @@ func (qs *QueueServer) Main() error {
 
 	err := <-exitCh
 	return err
+}
+
+func (qs *QueueServer) PubMessage(ctx context.Context, req *pb.PubMessageRequest) (*pb.PubMessageResponse, error) {
+	if req == nil || req.Pub == nil {
+		return nil, errInvalidPubRequest(req)
+	}
+	if req.Pub.Topic == "" {
+		return nil, errInvalidArgument("empty topic")
+	}
+
+	if len(req.Pub.Msg) == 0 {
+		return nil, errInvalidArgument("empty message")
+	}
+
+	log.Printf("[QueueServer] Incoming req: %+v\n", req)
+	return &pb.PubMessageResponse{}, nil
+}
+
+func (qs *QueueServer) SubQueue(req *pb.SubQueueRequest, srv pb.QueueService_SubQueueServer) error {
+	if req == nil || req.Sub == nil {
+		return errInvalidArgument("invalid sub request: %v", req)
+	}
+
+	topic := req.Sub.Topic
+	if topic == "" {
+		return errInvalidArgument("empty topic")
+	}
+
+	channel := req.Sub.Channel
+	if channel == "" {
+		return errInvalidArgument("empty channel")
+	}
+
+	srv.Send(&pb.QueueMsg{
+		Id:        "1",
+		Body:      "Hello",
+		Timestamp: time.Now().Unix(),
+	})
+	return nil
 }
