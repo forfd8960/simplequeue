a4c5d119a3dd40ce64f73a6c7333876ad18976be
forfd8960 <forfd8960@gmail.com>
2023-07-15 22:36:21 +0800

    Add grpc server main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,2 +1,36 @@
 package main
 
+import (
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+
+	"github.com/forfd8960/simplequeue"
+	"github.com/forfd8960/simplequeue/pb"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Printf("listen error: %v\n", err)
+		return
+	}
+
+	s := grpc.NewServer()
+
+	queueServer, err := simplequeue.NewQueueServer(&simplequeue.Options{})
+	if err != nil {
+		log.Printf("Init server err: %n", err)
+		os.Exit(1)
+	}
+
+	pb.RegisterQueueServiceServer(s, queueServer)
+
+	log.Println("-----------Start queueServer On 8080---------")
+	if err = s.Serve(lis); err != nil {
+		log.Printf("serve error: %v\n", err)
+		os.Exit(1)
+	}
+}
